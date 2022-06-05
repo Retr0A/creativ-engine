1e54b24f7d2913ad27e84d9bcdbb428755877b72
Alexander Buchkov <92212390+Retr0A@users.noreply.github.com>
2022-06-05 13:04:09 +0300

    Added Window

---
+++ Creativengine/Creativengine/src/Engine/EntryPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creativengine
+{
+    class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            Window.Create();
+        }
+    }
+}
---
+++ Creativengine/Creativengine/src/Graphics/Window.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Creativengine
+{
+    class Window
+    {
+        private static Form form;
+
+        public const int WIDTH = 1080;
+        public const int HEIGHT = 720;
+        public const string TITLE = "Creativengine";
+
+        public static void Create()
+        {
+            form = new Form() {
+                Width = WIDTH,
+                Height = HEIGHT,
+                Text = TITLE
+            };
+
+            Application.Run(form);
+        }
+
+        public static Form GetCurrentWindow()
+        {
+            return form;
+        }
+    }
+}
